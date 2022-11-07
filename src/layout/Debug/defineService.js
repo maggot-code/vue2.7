499072e513606c2ce40c29e9d5ee99e0b6a8f9c2
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-11-08 00:09:05 +0800

    fix :-1:

---
+++ src/layout/Debug/defineService.js
@@ -0,0 +1,18 @@
+/*
+ * @FilePath: /vue2.7/src/layout/Debug/defineService.js
+ * @Author: maggot-code
+ * @Date: 2022-11-07 22:00:10
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2022-11-07 22:16:24
+ * @Description: 
+ */
+function defineFetch() {
+    return {}
+}
+
+export function defineService(define) {
+    console.log(define);
+    return {}
+}
+
+export default defineService;
